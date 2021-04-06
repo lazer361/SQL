35b6e1ff2ddba7ca402b3cd18dec39c2eda28aa6
Alex361 <49017718+lazer361@users.noreply.github.com>
2021-04-06 17:16:06 +0300

    Add files via upload

--- user.sql
+++ user.sql
@@ -7,6 +7,18 @@ create table users (
         work_id  INTEGER
 );
 
+ALTER TABLE users
+ADD age int not null default 0; //Добавил поле Age
+
+ALTER TABLE users
+ADD age int not null default 0; //Пример изменения типа столбца
+
+ALTER TABLE users //Пример удаления стобца
+DROP COLUMN age
+
+ALTER TABLE timings //Пример изменения стобца (таблицы timings)
+RENAME COLUMN work_id TO user_id; 
+
 select * from users;
 
 INSERT INTO users (name, last_name, email, work_id) VALUES ('bob', 'lastBob' 'bob@mail.ru', 1);
